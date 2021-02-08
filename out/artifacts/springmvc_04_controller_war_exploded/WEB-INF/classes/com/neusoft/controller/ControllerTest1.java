6570a0d4c2053c025e144cd5c0fa0f84b07d1401
LuoTianyi712 <1228478412@qq.com>
2021-02-08 17:03:09 +0800

    模块4 Controller方法1 -- 实现接口方法开发

---
+++ out/artifacts/springmvc_04_controller_war_exploded/WEB-INF/classes/com/neusoft/controller/ControllerTest1.java
@@ -0,0 +1,20 @@
+package com.neusoft.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 只要实现了Controller接口的类，就说明这是一个控制器了
+public class ControllerTest1 implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.addObject("msg","Controller Test 1");
+        modelAndView.setViewName("test1");
+
+        return modelAndView;
+    }
+}
